e2d80bdaaaaf06110ddbee86344ad9dfa9ac3a21
morris88826 <morris88826@gmail.com>
2022-03-09 20:56:12 +0800

    add demo dataset

---
+++ demo.py
@@ -0,0 +1,52 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import json
+from libs.visualizer import plot_2d, plot_3d
+from PIL import Image
+
+if __name__ == "__main__":
+
+    class_name = "abseiling"
+    video_idx = 0
+    frame_idx = 0
+
+    with open("./dataset/info.json", 'r') as jsonfile:
+        info = json.load(jsonfile)
+
+    class_type = None
+    if(class_name in info["primary_classes"]):
+        class_type = "primary_classes"
+    elif(class_name in info["additional_classes"]):
+        class_type = "additional_classes"
+    else:
+        print("Action class not exists.")
+        raise NotImplementedError
+
+    skeleton_2d = np.load(
+        "./dataset/skeletons_2d/{}/{}/{}_{:03d}.npy".format(class_type, class_name, class_name, video_idx))[frame_idx]
+    skeleton_3d = np.load(
+        "./dataset/skeletons_3d/{}/{}/{}_{:03d}.npy".format(class_type, class_name, class_name, video_idx))[frame_idx]
+    image = np.array(Image.open(
+        "./dataset/raw/{}/{}_{:03d}/{:04d}.png".format(class_name, class_name, video_idx, frame_idx+1)))
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.imshow(image)
+    plot_2d(skeleton_2d, "haa4d", ax, xlim=None)
+    plt.show()
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection="3d")
+    plot_3d(skeleton_3d, "haa4d", ax, max_range=1000)
+    plt.show()
+
+    try: 
+        globally_aligned_skeletons = np.load(
+            "./dataset/processed_data/globally_aligned_skeletons/haa4d/{}/{}_{:03d}.npy".format(class_name, class_name, video_idx))[frame_idx]
+
+        fig = plt.figure()
+        ax = fig.add_subplot(111, projection="3d")
+        plot_3d(globally_aligned_skeletons, "haa4d", ax, max_range=1)
+        plt.show()
+    except:
+        print("Globally aligned skeleton not exist.")
---
+++ libs/visualizer.py
@@ -0,0 +1,42 @@
+from .skeleton import Skeleton
+
+def plot_2d(skeleton_2d, type, ax, xlim=[-1,1], ylim=[1,-1], xlabel='x', ylabel='y', title=''):
+    if xlim is not None and ylim is not None:
+        ax.set(xlim = xlim, ylim=ylim)
+    sk = Skeleton(type)
+
+    for body_part, joints in zip(sk.body_parts.keys(), sk.body_parts.values()):
+        for i in range(1, len(joints)):
+            start = sk.dict_joint_2_idx[joints[i-1]]
+            end = sk.dict_joint_2_idx[joints[i]]
+            x0, y0, = list(skeleton_2d[start])
+            x1, y1, = list(skeleton_2d[end])
+            ax.plot([x0, x1], [y0, y1], sk.body_parts_color[body_part])
+
+    # draw dots
+    for i in range(skeleton_2d.shape[0]):
+        ax.scatter(skeleton_2d[i, 0], skeleton_2d[i, 1], c='black', s=2)
+
+    ax.set(xlabel=xlabel, ylabel=ylabel, title=title)
+
+def plot_3d(skeleton_3d, type, ax, elev=-70, azim=-90,  max_range=1):
+
+    ax.set(xlim = [-max_range, max_range], ylim=[-max_range, max_range], zlim=[-max_range,max_range])
+    sk = Skeleton(type)
+
+    for body_part, joints in zip(sk.body_parts.keys(), sk.body_parts.values()):
+        for i in range(1, len(joints)):
+            start = sk.dict_joint_2_idx[joints[i-1]]
+            end = sk.dict_joint_2_idx[joints[i]]
+            x0, y0, z0 = list(skeleton_3d[start])
+            x1, y1, z1= list(skeleton_3d[end])
+            ax.plot([x0, x1], [y0, y1], [z0, z1], sk.body_parts_color[body_part])
+
+
+    # draw dots
+    for i in range(skeleton_3d.shape[0]):
+        ax.scatter(skeleton_3d[i, 0], skeleton_3d[i, 1], skeleton_3d[i, 2], c='black', s=2)
+
+            
+    ax.set(xlabel='x', ylabel='y', zlabel='z')
+    ax.view_init(elev=elev, azim=azim)
